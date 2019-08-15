971216e39084ea678b157b294179f019cc10c4c6
Andrey Morozov <32931484+AndreyMr@users.noreply.github.com>
2019-08-15 19:15:25 +0300

    added display authorname on main page and refactor Message class

--- src/main/java/com/config/WebSecurityConfig.java
+++ src/main/java/com/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.config;
 
+import com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,6 +16,8 @@ import javax.sql.DataSource;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
+    @Autowired
+    private UserService userService;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -40,7 +43,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //выбираем jdbc
+        auth.userDetailsService(userService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+
+        /*//выбираем jdbc
         auth.jdbcAuthentication()
         //вставляем нужный datasource (подключен из контекста через @Autowired)
                 .dataSource(dataSource)
@@ -49,6 +55,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         //делаем запрос по полям именно в данной последовательности (определено системой)
                 .usersByUsernameQuery("select username, password, active from springdb.usr where username= ?")
         //Spring получает список пользователей с ролями
-                .authoritiesByUsernameQuery("SELECT u.username, ur.roles FROM springdb.usr AS u INNER JOIN springdb.user_role AS ur ON u.id = ur.user_id WHERE username = ?");
+                .authoritiesByUsernameQuery("SELECT u.username, ur.roles FROM springdb.usr AS u INNER JOIN springdb.user_role AS ur ON u.id = ur.user_id WHERE username = ?");*/
     }
 }
--- src/main/java/com/entity/Message.java
+++ src/main/java/com/entity/Message.java
@@ -1,9 +1,7 @@
 package com.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Entity
 public class Message {
@@ -13,13 +11,22 @@ public class Message {
 
     private String messageText;
     private String tag;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
 
     public Message() {
     }
 
-    public Message(String message, String tag) {
+    public Message(String message, String tag, User author) {
+        this.author = author;
         this.messageText = message;
         this.tag = tag;
+    }
+    public String getAuthorName(){
+        if (author != null){
+            return author.getUsername();
+        } else return "<none>";
     }
 
     public Integer getId() {
@@ -44,5 +51,12 @@ public class Message {
 
     public void setTag(String tag) {
         this.tag = tag;
+    }
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 }
